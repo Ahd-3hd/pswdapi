87ee191d7b4d8201440eb0d4da461bd486d5fb69
ahd <ahdhani0@gmail.com>
2020-12-21 08:15:10 +0300

    install and enable cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("./api/routes/user.routes");
 const entityRouter = require("./api/routes/entity.routes");
 const mongoose = require("mongoose");
@@ -9,6 +10,7 @@ mongoose.connect(process.env.DB_URL, {
   useCreateIndex: true,
 });
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/auth", userRouter);
 app.use("/entities", entityRouter);
